e41e390978e531c2663f1f435c9752e31f5d9d68
rcrooks <rcrooks@roberts-mbp.home>
2013-11-27 09:56:57 -0500

    draft of batch callbacks doc

--- batch/callbacks/batch-callback.php
+++ batch/callbacks/batch-callback.php
@@ -21,7 +21,7 @@ $post_dump = print_r($_POST, TRUE);
 
 // Lastly, tell PHP where it can find the log file and tell PHP to open it
 // and add the string we created earlier to it.
-$logFileLocation = "log.txt";
+$logFileLocation = 'log.txt';
 $fileHandle = fopen($logFileLocation, 'a') or die("-1");
 fwrite($fileHandle, $logEntry);
 // also write the dump of the POST vars

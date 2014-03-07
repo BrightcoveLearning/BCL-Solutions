e6de1e4913364b3af67bfb334f567dd85f899fbc
doktorj7 <doktorjeff@Jeff-T-Doktors-iMac.local>
2014-03-07 14:38:26 -0700

    Updated plugin

--- video-js/new-player/plugins-doc-samples/multi-session-playback.js
+++ video-js/new-player/plugins-doc-samples/multi-session-playback.js
@@ -1,47 +1,37 @@
-videojs.plugin('firstPlugin', function() {
-  var myPlayer = this,
-    videoStart = 0,
-    currentPosition,
-    cookie = read_cookie("BC_position");
-  
-  if(cookie != null){
-    videoStart = cookie;
-  } else {
-    videoStart = 0;
-  }
-  
-  myPlayer.ready(function () {
-    //console.log("player loaded");
-    
-    myPlayer.on("loadedmetadata", function () {
-      //console.log("videoStart= " + cookie);
-      //document.getElementById("cookieDisplay1").innerHTML = read_cookie("BC_position");
-      if (videoStart > 0) {
-        myPlayer.currentTime(videoStart);
-        myPlayer.play();
-      }
-    });
-
-    myPlayer.on("timeupdate", onPosition);
-    
-    myPlayer.on("ended", function () {
-      videoStart = 0;
-      document.cookie="BC_position=0;";
-    });
-  });
-    
-  function onPosition(evt) {
-    //console.log(myPlayer.currentTime());
-    currentPosition = myPlayer.currentTime();
-    if (Math.round(currentPosition) != videoStart) {
-      videoStart = Math.round(currentPosition);
-      document.cookie="BC_position=" + escape(videoStart) + ";";
-      //document.getElementById("cookieDisplay2").innerHTML = read_cookie("BC_position");
-    }
-  }
-    
-  function read_cookie(key){
-    var result;
-    return (result = new RegExp('(^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? result[2] : null;
-  }
+videojs.plugin('multiSessionPlayback', function() {
+	var read_cookie = function(key){
+		var result;
+		return (result = new RegExp('(^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? result[2] : null;
+	}
+	
+	var myPlayer = this,
+		videoStart = 0,
+		currentPosition,
+		cookie = read_cookie("BC_position");
+	  
+	if(cookie != null){
+		videoStart = cookie;
+	} else {
+		videoStart = 0;
+	}
+		
+	myPlayer.on("loadedmetadata", function () {
+		if (videoStart > 0) {
+			myPlayer.currentTime(videoStart);
+			myPlayer.play();
+		}
+	})
+	
+	myPlayer.on("timeupdate", function() {
+		currentPosition = myPlayer.currentTime();
+		if (Math.round(currentPosition) != videoStart) {
+			videoStart = Math.round(currentPosition);
+			document.cookie="BC_position=" + escape(videoStart) + ";";
+		}
+	});
+	  
+	myPlayer.on("ended", function () {
+		videoStart = 0;
+		document.cookie="BC_position=0;";
+	})
 });

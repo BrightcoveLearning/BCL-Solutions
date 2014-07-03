8c451b2b2ea22ae918493ac2e83b96e658d60aeb
rcrooks <rcrooks@brightcove.com>
2014-07-03 15:08:46 -0400

    node app

--- oauther/oauther.js
+++ oauther/oauther.js
@@ -1,15 +1,14 @@
 var request = require('request');
 var client_id = "cd2392eb-de67-480c-b67b-2e00a2d28dab";
 var client_secret = "H_jedNY5aBUsR-ILm__L6qT-Nxhdel-6gkdlVJXbcjKmylJ3LIvBts2TjNibyBXfu9UaTk_HxbLZJsca2PeaKw";
-var requestBody = JSON.stringify({ "grant_type": "client_credentials",
-    "client_id": client_id,
-    "client_secret": client_secret
-  });
 
 request({
   method: 'POST',
   url: 'https://oauth.brightcove.com/v3/access_token',
-  body: requestBody
+  body: '{ "grant_type": client_credentials,
+    "client_id": client_id,
+    "client_secret": client_secret
+  }'
 }, function (error, response, body) {
   console.log('Status:', response.statusCode);
   console.log('Headers:', JSON.stringify(response.headers));

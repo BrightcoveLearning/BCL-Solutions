559c74b2add6ea855a2777e90e1e433b0019c3a6
rcrooks <rcrooks@roberts-mbp.home>
2014-07-03 06:55:59 -0400

    node work

--- oauther/app.js
+++ oauther/app.js
@@ -1,10 +1,15 @@
-var express = require('express'),
-    app = express(),
-    server = app.listen(3000, function() {
-        console.log('Listening on port %d', server.address().port);
-    });
-    
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
-});
+var request = require('request');
+var client_id = "cd2392eb-de67-480c-b67b-2e00a2d28dab";
+var client_secret = "H_jedNY5aBUsR-ILm__L6qT-Nxhdel-6gkdlVJXbcjKmylJ3LIvBts2TjNibyBXfu9UaTk_HxbLZJsca2PeaKw";
 
+request({
+  method: 'POST',
+  url: 'https://oauth.brightcove.com/v3/access_token',
+  body: "grant_type=client_credentials
+client_id=client_id
+client_secret=client_secret
+}, function (error, response, body) {
+  console.log('Status:', response.statusCode);
+  console.log('Headers:', JSON.stringify(response.headers));
+  console.log('Response:', body);
+});

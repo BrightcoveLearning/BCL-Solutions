b4b722d99e3ed7168dbcc41d3d75354c3d858e5b
rcrooks <rcrooks@roberts-mbp.home>
2014-07-11 10:13:52 -0400

    node work

--- bcls-proxy/app.js
+++ bcls-proxy/app.js
@@ -1,16 +1,44 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var util = require('util'),
+    http = require('http'),
+    colors = require('colors'),
+    httpProxy = require('http-proxy');
 
-server.listen(80);
+try {
+  var io = require('socket.io'),
+      client = require('socket.io-client');
+}
+catch (ex) {
+  console.error('Socket.io is required for this example:');
+  console.error('npm ' + 'install'.green);
+  process.exit(1);
+}
 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+//
+// Create the target HTTP server and setup
+// socket.io on it.
+//
+var server = io.listen(9014);
+server.sockets.on('connection', function (client) {
+  util.debug('Got websocket connection');
+
+  client.on('message', function (msg) {
+    util.debug('Got message from client: ' + msg);
+  });
+
+  client.send('from server');
 });
 
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-  });
+//
+// Create a proxy server with node-http-proxy
+//
+httpProxy.createServer({ target: 'ws://localhost:9014', ws: true }).listen(8014);
+
+//
+// Setup the socket.io client against our proxy
+//
+var ws = client.connect('ws://localhost:8014');
+
+ws.on('message', function (msg) {
+  util.debug('Got message: ' + msg);
+  ws.send('I am the client');
 });

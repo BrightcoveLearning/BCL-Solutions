39641226273a14a4fb27117c6869f88a1757b8a1
rcrooks <rcrooks@roberts-mbp.home>
2014-07-15 06:34:29 -0400

    proxy work

--- bcls-proxy/bcls-proxy.js
+++ bcls-proxy/bcls-proxy.js
@@ -1,17 +1,35 @@
 var util = require( 'util' ),
     colors = require( 'colors' ),
     http = require( 'http' ),
-    httpProxy = require( 'http-proxy' );
+    httpProxy = require( 'http-proxy' ),
+    bodyParamsArray = [],
+    options = {},
+    optionsTmp = {},
+    expiresTime = 0;
 
 //
 // Http Server with proxyRequest Handler and Latency
 //
 var proxy = new httpProxy.createProxyServer();
+console.log("proxy", proxy);
 http.createServer( function( req, res ) {
-    console.log( req.body );
-    proxy.web( req, res, {
-        target: 'http://localhost:9002'
+    console.log( req );
+    var body = '';
+    req.on( 'data', function( chunk ) {
+        body += chunk;
     } );
+    req.on( 'end', function( req, res ) {
+        console.log( body);
+        bodyParamsArray = body.split("&");
+        for (var i = 0; i < bodyParamsArray.length; i++) {
+            var tmpArray = bodyParamsArray[i].split("=");
+            optionsTmp[tmpArray[0]] = tmpArray[1];
+        }
+        // proxy.web( req, res, {
+        //     target: 'http://localhost:9002'
+        // } );
+    } );
+
 } ).listen( 8002 );
 
 //

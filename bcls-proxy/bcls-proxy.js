1a1ca3644c80045a30ed9eccc218c8575ab7ba9c
rcrooks <rcrooks@brightcove.com>
2014-07-21 14:49:53 -0400

    proxy

--- bcls-proxy/bcls-proxy.js
+++ bcls-proxy/bcls-proxy.js
@@ -1,101 +1,99 @@
 /*
-* bcls-proxy
-* Version: 0.1
-* Author: Robert Crooks
-* Description: Proxies Brightcove API requests, getting an access token, making the call, and returning
-* the response to an iframe on the client page
-* Requirements:
-*   POST your request to solutions.brightcove.com:8002, and target an iframe on your page
-*   Required fields for the body:
-*       client_id // (get from the Brightcove OAuth UI in Studio)
-*       client_secret // (get from the Brightcove OAuth UI in Studio)
-*       url // the full url for the API call you want to make, including parameters
-*       requestType // GET | POST | PUT | PATCH | DELETE
-*       requestBody // (optional) request body for calls that submit data
-*
-* Note: this is a sample only, not a supported Brightcove plugin
-* It only accepts requests from brightcove domains
-* If you would like to use the code to build your own proxy, see docs.brightcove.com/en/perform/oauth-api/guides/quick-start.html
-*/
-
-var util = require( "util" ),
-    colors = require( "colors" ),
-    csv = require("fast-csv"),
-    http = require( "http" ),
-    request = require( "request" ),
-    // functions
-    getFormValues,
-    getAccessToken,
-    sendRequest;
-
-/*
- * extract form values from request body
+ * bcls-proxy
+ * Version: 0.1
+ * Author: Robert Crooks
+ * Description: Proxies Brightcove API requests, getting an access token, making the call, and returning
+ * the response to an iframe on the client page
+ * Requirements:
+ *   POST your request to solutions.brightcove.com:8002, and target an iframe on your page
+ *   Required fields for the body:
+ *       client_id // (get from the Brightcove OAuth UI in Studio)
+ *       client_secret // (get from the Brightcove OAuth UI in Studio)
+ *       url // the full url for the API call you want to make, including parameters
+ *       requestType // GET | POST | PUT | PATCH | DELETE
+ *       requestBody // (optional) request body for calls that submit data
+ *
+ * Note: this is a sample only, not a supported Brightcove plugin
+ * It only accepts requests from brightcove domains
+ * If you would like to use the code to build your own proxy, see docs.brightcove.com/en/perform/oauth-api/guides/quick-start.html
  */
-getFormValues = function (body, callback) {
-    // split the request body string into an array
-    var valuesArray = body.split("&"),
-        options = {},
-        max = valuesArray.length,
-        i,
-        item,
-        error = null;
-    // initialize options to null
-    options.url = null;
-    options.client_id = null;
-    options.client_secret = null;
-    options.requestBody = null;
-    // now split each item into key and value and store in the object
-    for (i = 0; i < max; i++) {
-        item = valuesArray[i].split("=");
-        options[item[0]] = item[1];
-    }
-    // data fixes
-    // decode the URL
-    options.url = decodeURIComponent(options.url);
-    console.log("url", options.url);
-    // check for required values
-    if (options.client_id === null || options.client_secret === null) {
-        error = "Error: client_id and client_secret are required!";
-    }
-    if (error === null) {
-        callback(null, options);
-    } else {
-        callback(error);
-    }
-}
-
-/*
- * get new access_token
- */
-getAccessToken = function (options, callback) {
-    // base64 encode the ciient_id:client_secret string for basic auth
-    var auth_string = new Buffer(options.client_id + ":" + options.client_secret).toString("base64"),
-        bodyObj;
-    request({
-        method: 'POST',
-        url: 'https://oauth.brightcove.com/v3/access_token',
-        headers: {
-            "Authorization": "Basic " + auth_string,
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: 'grant_type=client_credentials'
-    }, function (error, response, body) {
-        // check for errors
+var BCLSPROXY = (function () {
+    "use strict";
+    var util = require("util"),
+        colors = require("colors"),
+        csv = require("fast-csv"),
+        http = require("http"),
+        request = require("request"),
+        // functions
+        getFormValues,
+        getAccessToken,
+        sendRequest;
+    /*
+     * extract form values from request body
+     */
+    getFormValues = function (body, callback) {
+        // split the request body string into an array
+        var valuesArray = body.split("&"),
+            options = {},
+            max = valuesArray.length,
+            i,
+            item,
+            error = null;
+        // initialize options to null
+        options.url = null;
+        options.client_id = null;
+        options.client_secret = null;
+        options.requestBody = null;
+        // now split each item into key and value and store in the object
+        for (i = 0; i < max; i = i + 1) {
+            item = valuesArray[i].split("=");
+            options[item[0]] = item[1];
+        }
+        // data fixes
+        // decode the URL
+        options.url = decodeURIComponent(options.url);
+        console.log("url", options.url);
+        // check for required values
+        if (options.client_id === null || options.client_secret === null) {
+            error = "Error: client_id and client_secret are required!";
+        }
         if (error === null) {
-            // return the access token to the callback
-            bodyObj = JSON.parse(body)
-            callback(null, bodyObj.access_token);
+            callback(null, options);
         } else {
             callback(error);
         }
-    });
-}
-
-/*
- * sends the request to the targeted API
- */
-sendRequest = function (token, options, callback) {
-    var requestOptions = {
+    };
+    /*
+     * get new access_token
+     */
+    getAccessToken = function (options, callback) {
+        // base64 encode the ciient_id:client_secret string for basic auth
+        var auth_string = new Buffer(options.client_id + ":" + options.client_secret).toString("base64"),
+            bodyObj;
+        request({
+            method: 'POST',
+            url: 'https://oauth.brightcove.com/v3/access_token',
+            headers: {
+                "Authorization": "Basic " + auth_string,
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: 'grant_type=client_credentials'
+        }, function (error, response, body) {
+            // check for errors
+            if (error === null) {
+                // return the access token to the callback
+                bodyObj = JSON.parse(body);
+                callback(null, bodyObj.access_token);
+            } else {
+                callback(error);
+            }
+        });
+    };
+    /*
+     * sends the request to the targeted API
+     */
+    sendRequest = function (token, options, callback) {
+        var requestOptions = {
             method: options.requestType,
             url: options.url,
             headers: {
@@ -104,36 +102,48 @@ sendRequest = function (token, options, callback) {
             },
             body: options.requestBody
         };
-    request(requestOptions, function (error, response, body) {
-        console.log('Error: ', error);
-        console.log('Status: ', response.statusCode);
-        console.log('Headers: ', JSON.stringify(response.headers, true, "  "));
-        console.log('Response: ', body);
-        if (error === null) {
-            callback(null, response, body);
-        } else {
-            callback(error);
-        }
-    });
-}
-
-/*
- * Http Server to handle requests
- */
-http.createServer( function( req, res ) {
-    console.log(req.headers);
-
+        request(requestOptions, function (error, response, body) {
+            console.log('Error: ', error);
+            console.log('Headers: ', response.headers);
+            console.log('Body: ', body);
+            if (error === null) {
+                if (response.headers["content-type"] === "text/csv;charset=UTF-8") {
+                    response.pipe(csv);
+                    csv.fromPath(body, {
+                        headers: true
+                    });
+                    csv.on("record", function (data) {
+                        console.log("csvdata ", data);
+                    });
+                    csv.on("end", function () {
+                        res.end(data);
+                    });
+                }
+                callback(null, response, body);
+            } else {
+                callback(error);
+            }
+        });
+    };
+    /*
+     * Http Server to handle requests
+     */
+    http.createServer(function (req, res) {
+        console.log(req.headers);
         var body = "";
-        req.on( "data", function( chunk ) {
+        if (req.headers.origin.indexOf("brightcove.com") < -1) {
+            res.writeHead(500);
+            res.end("Your request cannot be processed; this proxy only handles requests originating from Brightcove servers. If you would like to build your own version of this proxy, see http://docs.brightcove.com/en/perform/oauth-api/guides/quick-start.html");
+        }
+        req.on("data", function (chunk) {
             body += chunk;
-        } );
-        req.on( "end", function() {
-            if (req.headers.origin.indexOf("brightcove.com") > -1) {
-                console.log("body", body);
-                getFormValues(body, function (error, options) {
-                    if (error === null) {
-                        getAccessToken( options, function(err, token) {
-                            console.log("Access Token: ", token);
+        });
+        req.on("end", function () {
+            console.log("body", body);
+            getFormValues(body, function (error, options) {
+                if (error === null) {
+                    getAccessToken(options, function (error, token) {
+                        if (error === null) {
                             sendRequest(token, options, function (error, response, body) {
                                 if (error === null) {
                                     res.writeHead(200);
@@ -142,13 +152,7 @@ http.createServer( function( req, res ) {
                                         body = JSON.stringify(JSON.parse(body), true, 2);
                                         res.end(body);
                                     } else if (options.url.indexOf("format=csv") > -1) {
-                                        csv.fromPath(body, {headers: true})
-                                        csv.on("record", function(data){
-                                            console.log("csvdata ", data);
-                                        });
-                                        csv.on("end", function(){
-                                            res.end(data);
-                                        });
+
                                     } else {
                                         res.end("Your response will download automatically...");
                                     }
@@ -157,17 +161,17 @@ http.createServer( function( req, res ) {
                                     res.end("Your API call was unsuccessful; here is what the server returned: " + error);
                                 }
                             });
-                        });
-                    } else {
-                        res.writeHead(500, response.headers);
-                        res.end("There was a problem with your request: " + error);
-                    }
-                });
-            } else {
-                res.writeHead(500)
-                res.end("Your request cannot be processed; this proxy only handles requests originating from Brightcove servers. If you would like to build your own version of this proxy, see http://docs.brightcove.com/en/perform/oauth-api/guides/quick-start.html")
-            }
+                        } else {
+                            res.writeHead(500);
+                            res.end("There was a problem getting your access token: " + error);
+                        }
+                    });
+                } else {
+                    res.writeHead(500);
+                    res.end("There was a problem with your request: " + error);
+                }
+            });
         });
-} ).listen( 8002 );
-
-util.puts( "http server ".blue + "started ".green.bold + "on port ".blue + "8002 ".yellow);
+    }).listen(8002);
+    util.puts("http server ".blue + "started ".green.bold + "on port ".blue + "8002 ".yellow);
+})();

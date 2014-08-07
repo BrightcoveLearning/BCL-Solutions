db34c7421311bf18ef7af57cc65f02722af1da99
rcrooks <rcrooks@brightcove.com>
2014-08-07 12:48:53 -0400

    proxy update

--- bcls-proxy/bcls-proxy.js
+++ bcls-proxy/bcls-proxy.js
@@ -3,7 +3,7 @@
  * Version: 0.1.1
  ************
  * Change log
- * 0.0.1: added separate ports per API to be able to take advantage of token expire time
+ * 0.1.1: added separate ports per API to take advantage of token expire time
  ************
  * Author: Robert Crooks
  * Description: Proxies Brightcove API requests, getting an access token, making the call, and returning
@@ -12,6 +12,7 @@
  *   POST your request to:
  *      solutions.brightcove.com:8002 (for the Analytics API)
  *      solutions.brightcove.com:8003 (for the Player Management API)
+ *      solutions.brightcove.com:8004 (for other APIs)
  *   target an iframe on your page to display the response
  *   Required fields for the body:
  *       client_id // (get from the Brightcove OAuth UI in Studio)
@@ -33,13 +34,15 @@ var BCLSPROXY = (function () {
         aapiToken,
         pmapiToken,
         aapiExpires = 0,
-        pmapiExpires =0 ,
+        pmapiExpires = 0,
         aapiServer,
         pmapiServer,
+        apiServer, // for other APIs
         // functions
         getFormValues,
         getAAPIAccessToken,
         getPMAPIAccessToken,
+        getAccessToken,
         sendRequest;
     /*
      * extract form values from request body
@@ -143,6 +146,33 @@ var BCLSPROXY = (function () {
         }
     };
     /*
+     * get new access_token for other APIs
+     */
+    getAccessToken = function (options, callback) {
+        // base64 encode the ciient_id:client_secret string for basic auth
+        var auth_string = new Buffer(options.client_id + ":" + options.client_secret).toString("base64"),
+            bodyObj;
+        // don't know what API was requested, always get new token
+        request({
+            method: 'POST',
+            url: 'https://oauth.brightcove.com/v3/access_token',
+            headers: {
+                "Authorization": "Basic " + auth_string,
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: 'grant_type=client_credentials'
+        }, function (error, response, body) {
+            // check for errors
+            if (error === null) {
+                // return the access token to the callback
+                bodyObj = JSON.parse(body);
+                callback(null, bodyObj.access_token);
+            } else {
+                callback(error);
+            }
+        });
+    };
+    /*
      * sends the request to the targeted API
      */
     sendRequest = function (token, options, callback) {
@@ -190,8 +220,7 @@ sendRequest = function (token, options, callback) {
      * Http Server to handle Analytics API requests
      */
     aapiServer = http.createServer(function (req, res) {
-        var body = "",
-            now;
+        var body = "";
         /* the published version of this proxy accepts requests only from
          * domains that include "brightcove.com"
          * modify the following line to take requests from
@@ -242,7 +271,7 @@ sendRequest = function (token, options, callback) {
     // change the following line to have the proxy listen for requests on a different port
     }).listen(8002);
     /*
-     * Http Server to handle Player Mangement API requests
+     * Http Server to handle Player Management API requests
      */
     pmapiServer = http.createServer(function (req, res) {
         var body = "";
@@ -292,6 +321,58 @@ sendRequest = function (token, options, callback) {
         });
     // change the following line to have the proxy listen for requests on a different port
 }).listen(8003);
+    /*
+     * Http Server to handle other API requests
+     */
+    apiServer = http.createServer(function (req, res) {
+        var body = "";
+        // the published version of this proxy accepts requests only from domains that include "brightcove.com"
+        // modify the following line to take requests from other domains
+        // or remove the if block to accept requests from any domain (not recommended!)
+        if (req.headers.origin.indexOf("brightcove.com") < 0) {
+            res.writeHead(500);
+            res.end("Your request cannot be processed; this proxy only handles requests originating from Brightcove servers. If you would like to build your own version of this proxy, see http://docs.brightcove.com/en/perform/oauth-api/guides/quick-start.html");
+        }
+        req.on("data", function (chunk) {
+            body += chunk;
+        });
+        req.on("end", function () {
+            getFormValues(body, function (error, options) {
+                if (error === null) {
+                    getAccessToken(options, function (error, token) {
+                        if (error === null) {
+                            sendRequest(token, options, function (error, headers, body) {
+                                if (error === null) {
+                                    console.log("headers", headers);
+                                    var header;
+                                    for (header in headers) {
+                                        res.setHeader(header, headers[header]);
+                                    }
+                                    if (body.indexOf("{") === 0 || options.url.indexOf("format=json") > -1) {
+                                        // prettify JSON
+                                        body = JSON.stringify(JSON.parse(body), true, 2);
+                                    }
+                                        res.writeHead(200);
+                                        res.end(body);
+                                } else {
+                                    res.writeHead(500);
+                                    res.end("Your API call was unsuccessful; here is what the server returned: " + error);
+                                }
+                            });
+                        } else {
+                            res.writeHead(500);
+                            res.end("There was a problem getting your access token: " + error);
+                        }
+                    });
+                } else {
+                    res.writeHead(500);
+                    res.end("There was a problem with your request: " + error);
+                }
+            });
+        });
+    // change the following line to have the proxy listen for requests on a different port
+}).listen(8004);
     util.puts("http server for Analytics API ".blue + "started ".green.bold + "on port ".blue + "8002 ".yellow);
     util.puts("http server for Player Management API ".blue + "started ".green.bold + "on port ".blue + "8003 ".yellow);
+    util.puts("http server for other APIs ".blue + "started ".green.bold + "on port ".blue + "8004 ".yellow);
 })();

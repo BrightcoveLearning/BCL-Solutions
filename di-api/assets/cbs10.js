588a45d3a8ef94a66cfdcfa606738caf18837373
rcrooks <rcrooks@brightcove.com>
2014-10-23 13:53:09 -0400

    CBS finalized

---
+++ di-api/assets/cbs10.js
@@ -0,0 +1,10 @@
+var videoData = [];
+videoData[0] = [
+  {
+    "name": "NCIS_ep258_CIAN.mp4",
+    "ArchiveURL": "http://archive16.unicornmedia.com/archive/8511f5ed-db4c-41e1-80a0-de106794213f/C607018E-8B5B-11DD-B3ED-D99555D89593/90/52424a8b-d974-442b-9bc3-f8f7995cd584-0.mp4",
+    "description": "NCIS_ep258_CIAN.mp4",
+    "reference_id": "52424a8b-d974-442b-9bc3-f8f7995cd584",
+    "id": "3848030140001"
+  }
+];

bd6ea3ff4291f0769527d9e8f4a6d7df2e5f4380
rcrooks <rcrooks@brightcove.com>
2014-11-25 09:52:20 -0500

    cms-api solution

---
+++ cms-api/batch-update/assets/bcl-metadata-update.js
@@ -0,0 +1,20 @@
+var videoData = [
+     {
+          "name": "Betty Boop Full Movie",
+          "id": "96851865001",
+          "description": "",
+          "reference_id": "f4631c7c-a724-4226-920f-29de042e35c8"
+     },
+     {
+          "name": "Water-Liquid-Flow",
+          "id": "1194921693001",
+          "description": "Water flowing",
+          "reference_id": "f7d11d8b-d87a-499a-a64b-6ec14f243eb6"
+     },
+     {
+          "name": "Wildlife_Zebra",
+          "id": "734484324001",
+          "description": "The striped mammal",
+          "reference_id": "f4f8adef-2df8-49bf-84cd-95d31b20a7a2"
+     }
+];

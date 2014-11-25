c02c4beac13d7090a3899481a68b517efe0b1834
rcrooks <rcrooks@brightcove.com>
2014-11-25 16:41:52 -0500

    cms solution

---
+++ cms-api/batch-update/assets/cbs-updates2.js
@@ -0,0 +1,8 @@
+var videoData = [
+  {
+    "name": "BlindJustice_promo_1300K.wmv",
+    "id": "3896725558001",
+    "description": "Most officers injured in the line of duty opt for desk jobs or early retirement. Dunbar; however; has rehabilitated his body and attitude and fought his way back to active duty. His fresh start at a new precinct is threatened by the simple truth that no o",
+    "reference_id": "f91e0e39-25d0-49b4-990c-1ad25870405b"
+  }
+];
---
+++ cms-api/batch-update/cbs-updates2.js
@@ -0,0 +1,110 @@
+var videoData = [
+  {
+    "name": "60_10212012_CIAN_DSI-30-16x9.mp4",
+    "id": "3896587264001",
+    "description": "",
+    "reference_id": "f4f8adef-2df8-49bf-84cd-95d31b20a7a2"
+  },
+  {
+    "name": "60_11112012_FULL_DSI-30-16x9_464.mp4",
+    "id": "3896605886001",
+    "description": "",
+    "reference_id": "fdd68880-8132-4dec-84c4-993d82742422"
+  },
+  {
+    "name": "GAME_ep045_CIAN.mp4",
+    "id": "3896605856001",
+    "description": "",
+    "reference_id": "f6133349-7f18-403f-92a3-05b047dc4ecb"
+  },
+  {
+    "name": "MADAM_SECRETARY_ep105_CIAN.mp4",
+    "id": "3896587180001",
+    "description": "",
+    "reference_id": "30230232-b5c3-40b0-8d83-30d8f9d0afdc"
+  },
+  {
+    "name": "NCIS_ep261_CIAN.mp4",
+    "id": "3896124223001",
+    "description": "",
+    "reference_id": "5a4ca5f4-6bfb-4acf-aae9-bf58a0e87b2c"
+  },
+  {
+    "name": "ODD-Matthew Greeting 2.mp4",
+    "id": "3896124200001",
+    "description": "",
+    "reference_id": "09947560-cf80-4b35-91ac-9354fab78208"
+  },
+  {
+    "name": "OneTrueLove_Promo_1M.wmv",
+    "id": "3896124259001",
+    "description": "Two strangers meet at the site of a serious car accident; and in one life-altering moment; they fall in love��espite the fact that they are both engaged to be married to their long-time partners.",
+    "reference_id": "ee75b7d7-b98f-435e-a89d-9e3e94635d88"
+  },
+  {
+    "name": "OnlineNation_ep102_1300K_4x3_30.wmv",
+    "id": "3896587266001",
+    "description": "",
+    "reference_id": "f5b7c967-8cfe-4742-8679-e4d9c6c46ad4"
+  },
+  {
+    "name": "Survivor_Ep2901_1300K_16x9_30.wmv",
+    "id": "3896587221001",
+    "description": "",
+    "reference_id": "de1ffc22-c1c1-4f25-ab9e-f03791fb381e"
+  },
+  {
+    "name": "SURVIVOR_ep2906_CIAN.mp4",
+    "id": "3896587194001",
+    "description": "",
+    "reference_id": "65b95bd2-30bc-4a1c-b674-27987efb0446"
+  },
+  {
+    "name": "Survivor_Ep301_1300K_4x3_30.wmv",
+    "id": "3896725541001",
+    "description": "",
+    "reference_id": "f6a7e763-7d5a-4752-9037-1975b3f54507"
+  },
+  {
+    "name": "SV24_s24_CastawaysEPK-I60-PKG_1300K_16x9.wmv",
+    "id": "3896140779001",
+    "description": "",
+    "reference_id": "36390225-1517-429f-b7bc-c1dcc36adcb7"
+  },
+  {
+    "name": "BlindJustice_promo_1300K.wmv",
+    "id": "3896725558001",
+    "description": "Most officers injured in the line of duty opt for desk jobs or early retirement. Dunbar; however; has rehabilitated his body and attitude and fought his way back to active duty. His fresh start at a new precinct is threatened by the simple truth that no o",
+    "reference_id": "f91e0e39-25d0-49b4-990c-1ad25870405b"
+  },
+  {
+    "name": "BradyBunch_Ep091_1300K_4x3_24.wmv",
+    "id": "3896605892001",
+    "description": "The classic 'Ooh; my nose!' episode. Marcia; wanting to date the school hunk instead of the nice but unspectacular son of a wallpaper salesman; breaks her date. Peter's errantly thrown football causes Marcia's nose to swell and her dream date to unravel.",
+    "reference_id": "febe801f-98b0-4706-8274-3881cd4b5061"
+  },
+  {
+    "name": "ChrisIsaakShow_Ep034_1M.wmv",
+    "id": "3896605666001",
+    "description": "At the request of his mother; Dr. Dorothy Isaak; Chris agrees to organize a charity benefit concert at Bimbos. The night of the event; during a torrential rain storm; he shares the stage with an amazing line-up of talent  among them Green Day; The Goo Go",
+    "reference_id": "f8f5a5b8-daed-496f-bc96-af0f861f0e00"
+  },
+  {
+    "name": "ExtraordinaryMeasures_dtlr1_9000K_16x9_24.wmv",
+    "id": "3896587251001",
+    "description": "In the tradition of great inspiration dramas like the Academy Award nominated film Erin Brockovich and The Pursuit of Happyness; EXTRAORDINARY MEASURES is inspired by the true story of John Crowley; a man who defied conventional wisdom and great odds; and",
+    "reference_id": "f21c8df5-03c2-4d85-a576-0052a05739dc"
+  },
+  {
+    "name": "IGetThatALot_101_1300K_4x3_30.wmv",
+    "id": "3896605691001",
+    "description": "I GET THAT A LOT features celebrities tricking everyday citizens in a case of supposed 'mistaken identity.'  Working ordinary jobs; these celebrities confuse customers who can't decide if it's the actual celebrity or an astonishing look-alike. As these ce",
+    "reference_id": "ffb2ed4f-4092-4456-a8a2-0f1e54b7a0f3"
+  },
+  {
+    "name": "ILoveLucy_Ep150_1300K_4x3_24.wmv",
+    "id": "3896605686001",
+    "description": "En route to Rome by train; Lucy is spotted by a famous Italian cinema director and chosen to play a part in his new movie 'Bitter Grapes.' Lucy sets out to immerse herself in the role. When she nonchalantly wanders into a vineyard inhabited by a motley as",
+    "reference_id": "feaa8687-3909-4fdb-9ffd-3ec4f0b3a39a"
+  }
+];

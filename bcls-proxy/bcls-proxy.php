c76ca6f7d82ec8e7526c45287f952e64b2c9032b
rcrooks <rcrooks@brightcove.com>
2015-03-27 16:07:57 -0400

    work on php proxy

--- bcls-proxy/bcls-proxy.php
+++ bcls-proxy/bcls-proxy.php
@@ -1,12 +1,12 @@
 <?php
 
-	/*!
+  /*!
   | @ Sends the request
   | @param $url string API request url
   | @param $method string HTTP method for the
   | @return string
   */
-  function SendRequest( $url, $method = "GET", $data = array(), $headers = array("Content-type: application/x-www-form-urlencoded") )
+  function GetToken( $url, $method, $data, $headers )
   {
     $context = stream_context_create(array
     (
@@ -18,16 +18,50 @@
     ));
     return file_get_contents($url, false, $context);
   }
-  // no data to submit
-  $data = array();
-  // get current time and 24 hours ago in milliseconds
-  $to = time() * 1000;
-  $from = $to - (24 * 60 * 60 * 1000);
-  // get the URL and authorization info from the form data
-  $request = "https://data.brightcove.com/analytics-api/videocloud/account/20318290001/report/?dimensions=video&limit=6&sort=video_view" . "&from=" . $from . "&to=" . $to;
+	/*!
+  | @ Sends the request
+  | @param $url string API request url
+  | @param $method string HTTP method for the
+  | @return string
+  */
+  function SendRequest( $url, $method = "GET", $data = array(), $headers = array("Content-type: application/json") )
+  {
+    $context = stream_context_create(array
+    (
+      "http" => array(
+        "method" => $method,
+        "header" => $headers,
+        "content" => http_build_query( $data )
+      )
+    ));
+    return file_get_contents($url, false, $context);
+  }
+
+  // request params from the $_POST
+  $request = $_POST["url"];
+  $client_id = $_POST["client_id"];
+  $client_secret = $_POST["client_secret"];
+  $data = $_POST["requestBody"];
+  $requestType = $_POST["requestType"];
+  // create the auth string to get a token
+  $authStr = base64_encode($client_id . ":" . $client_secret);
+
+  // set up auth params for getting token
+  $authURL = "https://oauth.brightcove.com/v3/access_token";
+  $authBody = array(
+      "grant_type" => "client_credentials"
+    );
+  $authHeaders = array(
+    "Content-type: application/x-www-form-urlencoded",
+    "Authorization: Basic" . $authStr
+    );
+  $authMethod = "POST";
+
+  $authReponse = GetToken($authURL, $authMethod, $authHeaders);
+  $authReponse = json_decode($authReponse);
   // add headers
   $headers = array(1 => "Authorization: Bearer 15075c51ae4b0af095c9a619a");
   //send the http request
-  $result = SendRequest($request, $method = "GET", $data, $headers);
+  $result = SendRequest($request, $requestType, $data, $headers);
   echo "JSONresponse = " . $result;
 ?>

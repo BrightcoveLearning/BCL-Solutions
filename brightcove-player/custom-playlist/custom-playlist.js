0893e790445118fbb8794494e9f09a1dc411d672
rcrooks <rcrooks@brightcove.com>
2015-09-12 19:16:27 -0400

    updates

---
+++ brightcove-player/custom-playlist/custom-playlist.js
@@ -0,0 +1,64 @@
+videojs.plugin('customPlaylist', function() {
+    var myPlayer = this;
+    // handle loadedmetadata just once, it fires again with each video load
+    myPlayer.one('loadedmetadata', function () {
+        var playerEl = myPlayer.el(),
+            playerParent = playerEl.parentNode,
+            i,
+            iMax,
+            playerWrapper = document.createElement('div'),
+            playlistWrapper = document.createElement('div'),
+            playlistItem,
+            playlistItems,
+            thumbnailImg,
+            playlistData = myPlayer.playlist(),
+            videoItem;
+        // function to clear highlighting of list items
+        function clearHighlight() {
+            iMax = playlistItems.length;
+            for (i = 0; i < iMax; i++) {
+                playlistItems[i].setAttribute('class', 'bcls-thumbnail');
+            }
+        }
+
+        // function to set highlight
+        function setHighlight() {
+            playlistItems[myPlayer.currentItem()].setAttribute('class', 'bcls-highlight');
+        }
+
+        // function to load playlist items on click
+        function loadPlaylistItem() {
+            var index = this.getAttribute('data-playlist-index');
+            myPlayer.playlist.currentItem(parseInt(index, 10));
+            myPlayer.play();
+        }
+        console.log(myPlayer.playlist());
+        playerParent.insertBefore(playerWrapper, playerEl);
+        playerWrapper.appendChild(playerEl);
+        playerWrapper.appendChild(playlistWrapper);
+        iMax = playlistData.length;
+        for (i = 0; i < iMax; i++) {
+            videoItem = playlistData[i];
+            // create the playlist item and set its class
+            playlistItem = document.createElement('div');
+            playlistItem.setAttribute('class', 'bcls-playlist-item');
+            // create the thumbnail img and set its class
+            thumbnailImg = document.createElement('img');
+            thumbnailImg.setAttribute('class', 'bcls-thumbnail');
+            // set the src for the thumbnail
+            thumbnailImg.setAttribute('src', videoItem.thumbnail);
+            // save the index - need this to load the video
+            thumbnailImg.setAttribute('data-playlist-index', i);
+            // for best practice, set the alt attribute to the video name
+            thumbnailImg.setAttribute('alt', videoItem.name);
+            // now append the img to the item, and the item to the playlist
+            playlistItem.appendChild(thumbnailImg);
+            playlistWrapper.appendChild(playlistItem);
+        }
+        playlistItems = document.getElementsByClassName('bcls-thumbnail');
+        iMax = playlistItems.length;
+        for (i = 0; i < iMax; i++) {
+            playlistItems[i].addEventListener('click', loadPlaylistItem);
+        }
+    });
+});

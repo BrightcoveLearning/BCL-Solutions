e025b3b3d268ed6355a735802832b6c84c39d59e
Robert Crooks <rcrooks@Roberts-MacBook-Pro.local>
2015-10-18 13:25:33 -0400

    customer sample and proxy for apidocs

---
+++ angular-sample/angular-load.js
@@ -0,0 +1,65 @@
+/* angular-load.js / v0.3.0 / (c) 2014, 2015 Uri Shaked / MIT Licence */
+
+(function () {
+	'use strict';
+
+	angular.module('angularLoad', [])
+		.service('angularLoad', ['$document', '$q', '$timeout', function ($document, $q, $timeout) {
+
+			function loader(createElement) {
+				var promises = {};
+
+				return function(url) {
+					if (typeof promises[url] === 'undefined') {
+						var deferred = $q.defer();
+						var element = createElement(url);
+
+						element.onload = element.onreadystatechange = function (e) {
+							$timeout(function () {
+								deferred.resolve(e);
+							});
+						};
+						element.onerror = function (e) {
+							$timeout(function () {
+								deferred.reject(e);
+							});
+						};
+
+						promises[url] = deferred.promise;
+					}
+
+					return promises[url];
+				};
+			}
+
+			/**
+			 * Dynamically loads the given script
+			 * @param src The url of the script to load dynamically
+			 * @returns {*} Promise that will be resolved once the script has been loaded.
+			 */
+			this.loadScript = loader(function (src) {
+				var script = $document[0].createElement('script');
+
+				script.src = src;
+
+				$document[0].body.appendChild(script);
+				return script;
+			});
+
+			/**
+			 * Dynamically loads the given CSS file
+			 * @param href The url of the CSS to load dynamically
+			 * @returns {*} Promise that will be resolved once the CSS file has been loaded.
+			 */
+			this.loadCSS = loader(function (href) {
+				var style = $document[0].createElement('link');
+
+				style.rel = 'stylesheet';
+				style.type = 'text/css';
+				style.href = href;
+
+				$document[0].head.appendChild(style);
+				return style;
+			});
+		}]);
+})();
---
+++ bcls-proxy/apidoc-proxy.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * bcls-proxy.php - proxy for Brightcove RESTful APIs
+ * gets an access token, makes the request, and returns the response
+ * Accessing:
+ *     URL: https://solutions.brightcove.com/bcls/bcls-proxy/bcsl-proxy.php
+ *         (note you should *always* access the proxy via HTTPS)
+ *     Method: POST
+ *
+ * @post {string} url - the URL for the API request
+ * @post {string} [requestType=GET] - HTTP method for the request
+ * @post {string} [requestBody=null] - JSON data to be sent with write requests
+ * @post {string} client_id - OAuth2 client id with sufficient permissions for the request
+ * @post {string} client_secret - OAuth2 client secret with sufficient permissions for the request
+ *
+ * @returns {string} $response - JSON response received from the API
+ */
+
+// CORS enablement
+header("Access-Control-Allow-Origin: *");
+
+// set up request for access token
+$data = array();
+
+$client_id     = '553d4903-4547-435d-944c-2c8e2f6abc5d';
+$client_secret = 'ENBQH6pHfJQub7oR0SGCn2Pu_W2SY5QsVw24fK-frXcE6hdTRnJO-0_LBmKZh15rVliIAiECAQF1yBYP_l90gQ';
+$auth_string   = "{$client_id}:{$client_secret}";
+$request       = "https://oauth.brightcove.com/v3/access_token?grant_type=client_credentials";
+$ch            = curl_init($request);
+curl_setopt_array($ch, array(
+		CURLOPT_POST           => TRUE,
+		CURLOPT_RETURNTRANSFER => TRUE,
+		CURLOPT_SSL_VERIFYPEER => FALSE,
+		CURLOPT_USERPWD        => $auth_string,
+		CURLOPT_HTTPHEADER     => array(
+			'Content-type: application/x-www-form-urlencoded',
+		),
+		CURLOPT_POSTFIELDS => $data
+	));
+$response = curl_exec($ch);
+curl_close($ch);
+
+// Check for errors
+if ($response === FALSE) {
+	die(curl_error($ch));
+}
+
+// Decode the response
+$responseData = json_decode($response, TRUE);
+$access_token = $responseData["access_token"];
+
+// set up the API call
+// get data
+if ($_POST["requestBody"]) {
+	$data = json_decode($_POST["requestBody"]);
+} else {
+	$data = array();
+}
+// get request type or default to GET
+if ($_POST["requestType"]) {
+	$method = $_POST["requestType"];
+} else {
+	$method = "GET";
+}
+
+// get the URL and authorization info from the form data
+$request = $_POST["url"];
+
+//send the http request
+$ch = curl_init($request);
+curl_setopt_array($ch, array(
+		CURLOPT_CUSTOMREQUEST  => $method,
+		CURLOPT_RETURNTRANSFER => TRUE,
+		CURLOPT_SSL_VERIFYPEER => FALSE,
+		CURLOPT_HTTPHEADER     => array(
+			'Content-type: application/json',
+			"Authorization: Bearer {$access_token}",
+		),
+		CURLOPT_POSTFIELDS => json_encode($data)
+	));
+$response = curl_exec($ch);
+curl_close($ch);
+
+// Check for errors
+if ($response === FALSE) {
+	$logEntry = "\nError:\n".
+	"\n".date("Y-m-d H:i:s")." UTC \n"
+	.$response;
+	$logFileLocation = "log.txt";
+	$fileHandle      = fopen($logFileLocation, 'a') or die("-1");
+	fwrite($fileHandle, $logEntry);
+	fclose($fileHandle);
+	echo "Error: there was a problem with your API call"+
+	die(curl_error($ch));
+}
+
+// Decode the response
+// $responseData = json_decode($response, TRUE);
+// return the response to the AJAX caller
+echo $response;
+?>

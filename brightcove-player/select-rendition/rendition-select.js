06581b7d921c66c04dad91c1f7f4fbaec01a30d6
rcrooks <rcrooks@brightcove.com>
2015-11-17 14:46:25 -0500

    plugin

---
+++ brightcove-player/select-rendition/rendition-select.js
@@ -0,0 +1,27 @@
+videojs.plugin('downloadVideoPlugin', function() {
+    var myPlayer = this,
+        videoID,
+        videoName,
+        totalRenditions,
+        mp4Ara = [],
+        controlBar,
+        newElement;
+
+    videoID = myPlayer.options()['data-video-id'];
+    myPlayer.catalog.getVideo(videoID, function(error, video) {
+        myPlayer.catalog.load(video);
+        videoName = myPlayer.mediainfo['name'];
+        rendtionsAra = myPlayer.mediainfo.sources;
+        totalRenditions = rendtionsAra.length;
+        for (var i = 0; i < totalRenditions; i++) {
+            if (rendtionsAra[i].container === "MP4" && rendtionsAra[i].hasOwnProperty('src')) {
+                mp4Ara.push(rendtionsAra[i]);
+            };
+        };
+        mp4Ara.sort(function(a, b) {
+            return b.size - a.size;
+        });
+        highestQuality = mp4Ara[0].src;
+        myPlayer.src(highestQuality);
+    });
+});

6b7f6fc75ddbaad651eb68c84817994d70319f3f
rcrooks <rcrooks@brightcove.com>
2015-11-21 07:57:07 -0500

    work on transceiver

--- di-transceiver/routes/index.js
+++ di-transceiver/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    request = require("request"),
+    requestQueue = [],
+    runningJobCount = 0;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* POST notifications. */
+router.post('/notifications', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* POST requests. */
+router.post('/requests', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 
 module.exports = router;

5b10f6477c2e73744ee148f06b7ab112f6444ed6
rcrooks <rcrooks@brightcove.com>
2015-11-21 08:00:57 -0500

    transceiver

--- di-transceiver/routes/index.js
+++ di-transceiver/routes/index.js
@@ -6,17 +6,25 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 /* POST notifications. */
 router.post('/notifications', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 /* POST requests. */
 router.post('/requests', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 

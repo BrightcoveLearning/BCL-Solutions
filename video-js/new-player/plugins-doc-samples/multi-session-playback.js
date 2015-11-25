5fa28eb1eac6d7163bdb20f3d871986f9d978cd2
Matthew Boles <mboles@brightcove.com>
2015-11-25 13:24:11 -0500

    Indentation clean up

--- video-js/new-player/plugins-doc-samples/multi-session-playback.js
+++ video-js/new-player/plugins-doc-samples/multi-session-playback.js
@@ -1,37 +1,32 @@
-videojs.plugin('multiSessionPlayback', function() {
-    var read_cookie = function(key){
-  var result;
-  return (result = new RegExp('(^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? result[2] : null;
-    }
-
-    var myPlayer = this,
-  videoStart = 0,
-  currentPosition,
-  cookie = read_cookie("BC_position");
-
-    if(cookie != null){
-  videoStart = cookie;
-    } else {
-  videoStart = 0;
-    }
-
-    myPlayer.on("loadedmetadata", function () {
-  if (videoStart > 0) {
+videojs.plugin('multiSessionPlayback', function () {
+  var read_cookie = function (key) {
+    var result;
+    return (result = new RegExp('(^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? result[2] : null;
+  }
+  var myPlayer = this,
+    videoStart = 0,
+    currentPosition,
+    cookie = read_cookie("BC_position");
+  if (cookie != null) {
+    videoStart = cookie;
+  } else {
+    videoStart = 0;
+  }
+  myPlayer.on("loadedmetadata", function () {
+    if (videoStart > 0) {
       myPlayer.currentTime(videoStart);
       myPlayer.play();
-  }
-    })
-
-    myPlayer.on("timeupdate", function() {
-  currentPosition = myPlayer.currentTime();
-  if (Math.round(currentPosition) != videoStart) {
+    }
+  })
+  myPlayer.on("timeupdate", function () {
+    currentPosition = myPlayer.currentTime();
+    if (Math.round(currentPosition) != videoStart) {
       videoStart = Math.round(currentPosition);
-      document.cookie="BC_position=" + escape(videoStart) + ";";
-  }
-    });
-
-    myPlayer.on("ended", function () {
-  videoStart = 0;
-  document.cookie="BC_position=;expires=0";
-    })
+      document.cookie = "BC_position=" + escape(videoStart) + ";";
+    }
+  });
+  myPlayer.on("ended", function () {
+    videoStart = 0;
+    document.cookie = "BC_position=;expires=0";
+  })
 });

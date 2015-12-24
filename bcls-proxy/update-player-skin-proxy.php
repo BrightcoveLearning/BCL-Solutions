f2ff12c6f23b5c96db6b634180566f08b612602b
rcrooks <rcrooks@brightcove.com>
2015-12-24 15:57:59 -0500

    finished player skin updater

--- bcls-proxy/update-player-skin-proxy.php
+++ bcls-proxy/update-player-skin-proxy.php
@@ -24,6 +24,7 @@ $username     = $_POST["username"];
 $password     = $_POST["password"];
 $account_id   = $_POST["account_id"];
 $player_id    = $_POST["player_id"];
+$publish      = $_POST["publish"];
 $url = 'https://players.api.brightcove.com/v1/accounts/'.$account_id.'/players/'.$player_id.'/configuration';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -72,31 +73,37 @@ if ($response === FALSE) {
     echo $status_code;
 }
 $playerConfig = json_decode($response);
-// echo json_encode($playerConfig) . PHP_EOL;
 
 // now publish the player
 
 $url = 'https://players.api.brightcove.com/v1/accounts/'.$account_id.'/players/'.$player_id.'/publish';
+$headers = array('Content-Type: application/json');
 
+if ($publish == "true") {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$response = curl_exec($ch);
-curl_close($ch);
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $response = curl_exec($ch);
+    curl_close($ch);
 
-// Check for errors
-if ($response === FALSE) {
-    die(curl_error($ch));
-    echo $status_code;
+    // Check for errors
+    if ($response === FALSE) {
+        die(curl_error($ch));
+        echo $status_code;
+    }
+    $playerConfig = json_decode($response);
+    echo json_encode($playerConfig) . PHP_EOL;
+} else {
+    echo json_encode($playerConfig) . PHP_EOL;
 }
-$playerConfig = json_decode($response);
-echo json_encode($playerConfig) . PHP_EOL;
+
 
 ?>

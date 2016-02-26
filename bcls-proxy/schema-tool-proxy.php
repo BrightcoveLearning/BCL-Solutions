1dfc07da869f9732993a0d533f6f4dc84d3758f4
rcrooks <rcrooks@brightcove.com>
2016-02-26 06:20:49 -0500

    updated some assets

--- bcls-proxy/schema-tool-proxy.php
+++ bcls-proxy/schema-tool-proxy.php
@@ -23,35 +23,35 @@ header("Access-Control-Allow-Origin: *");
 $data = array();
 
 if ($_POST["client_id"]) {
-	$client_id = $_POST["client_id"];
+    $client_id = $_POST["client_id"];
 } else {
-	$client_id = 'bc90ea5e-8a66-476b-8c43-90bf28215ed1';
+    $client_id = 'c5d0a622-5479-46d8-8d8a-5f034b943fab';
 }
 if ($_POST["client_secret"]) {
-	$client_secret = $_POST["client_secret"];
+    $client_secret = $_POST["client_secret"];
 } else {
-	$client_secret = 'hPvmIPc40FOwcS14Jg7_rZjEM-xpRk-8MJO507ZGRKe2eOXKzsdp8nnKLSgTOtbvqsn4v8ZVMT6mViyA3D7jDg';
+    $client_secret = 'w7NQYu0vUloM4GYYy2SXAxrvyFpt8fwI35qAFZcS13-VIgs0itwKNsAwHOS80sOWKJ1BUwHIvSFG2IbgcxEGKg';
 }
 
 $auth_string = "{$client_id}:{$client_secret}";
 $request     = "https://oauth.brightcove.com/v3/access_token?grant_type=client_credentials";
 $ch          = curl_init($request);
 curl_setopt_array($ch, array(
-		CURLOPT_POST           => TRUE,
-		CURLOPT_RETURNTRANSFER => TRUE,
-		CURLOPT_SSL_VERIFYPEER => FALSE,
-		CURLOPT_USERPWD        => $auth_string,
-		CURLOPT_HTTPHEADER     => array(
-			'Content-type: application/x-www-form-urlencoded',
-		),
-		CURLOPT_POSTFIELDS => $data
-	));
+        CURLOPT_POST           => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_SSL_VERIFYPEER => FALSE,
+        CURLOPT_USERPWD        => $auth_string,
+        CURLOPT_HTTPHEADER     => array(
+            'Content-type: application/x-www-form-urlencoded',
+        ),
+        CURLOPT_POSTFIELDS => $data
+    ));
 $response = curl_exec($ch);
 curl_close($ch);
 
 // Check for errors
 if ($response === FALSE) {
-	die(curl_error($ch));
+    die(curl_error($ch));
 }
 
 // Decode the response
@@ -61,15 +61,15 @@ $access_token = $responseData["access_token"];
 // set up the API call
 // get data
 if ($_POST["requestBody"]) {
-	$data = json_decode($_POST["requestBody"]);
+    $data = json_decode($_POST["requestBody"]);
 } else {
-	$data = array();
+    $data = array();
 }
 // get request type or default to GET
 if ($_POST["requestType"]) {
-	$method = $_POST["requestType"];
+    $method = $_POST["requestType"];
 } else {
-	$method = "GET";
+    $method = "GET";
 }
 
 // get the URL and authorization info from the form data
@@ -78,29 +78,29 @@ $request = $_POST["url"];
 //send the http request
 $ch = curl_init($request);
 curl_setopt_array($ch, array(
-		CURLOPT_CUSTOMREQUEST  => $method,
-		CURLOPT_RETURNTRANSFER => TRUE,
-		CURLOPT_SSL_VERIFYPEER => FALSE,
-		CURLOPT_HTTPHEADER     => array(
-			'Content-type: application/json',
-			"Authorization: Bearer {$access_token}",
-		),
-		CURLOPT_POSTFIELDS => json_encode($data)
-	));
+        CURLOPT_CUSTOMREQUEST  => $method,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_SSL_VERIFYPEER => FALSE,
+        CURLOPT_HTTPHEADER     => array(
+            'Content-type: application/json',
+            "Authorization: Bearer {$access_token}",
+        ),
+        CURLOPT_POSTFIELDS => json_encode($data)
+    ));
 $response = curl_exec($ch);
 curl_close($ch);
 
 // Check for errors
 if ($response === FALSE) {
-	$logEntry = "\nError:\n".
-	"\n".date("Y-m-d H:i:s")." UTC \n"
-	.$response;
-	$logFileLocation = "log.txt";
-	$fileHandle      = fopen($logFileLocation, 'a') or die("-1");
-	fwrite($fileHandle, $logEntry);
-	fclose($fileHandle);
-	echo "Error: there was a problem with your API call"+
-	die(curl_error($ch));
+    $logEntry = "\nError:\n".
+    "\n".date("Y-m-d H:i:s")." UTC \n"
+    .$response;
+    $logFileLocation = "log.txt";
+    $fileHandle      = fopen($logFileLocation, 'a') or die("-1");
+    fwrite($fileHandle, $logEntry);
+    fclose($fileHandle);
+    echo "Error: there was a problem with your API call"+
+    die(curl_error($ch));
 }
 
 // Decode the response

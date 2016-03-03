1a7a4983fb2c9af37c518021bff94b675768b7f0
Matthew Boles <mboles@brightcove.com>
2016-03-03 09:40:09 -0500

    latest work

--- brightcove-player/download-video/5x-download-video.js
+++ brightcove-player/download-video/5x-download-video.js
@@ -1,34 +1,46 @@
 videojs.plugin('downloadVideoPlugin', function() {
   var myPlayer = this,
-    videoID,
-    videoName,
-    totalRenditions,
-    mp4Ara=[],
-    highestQuality,
-    controlBar,
-    newElement;
-    
-  videoID = myPlayer.options()['data-video-id'];
-  myPlayer.catalog.getVideo(videoID, function(error, video) {
-    myPlayer.catalog.load(video);
+  videoID,
+  videoName,
+  totalRenditions,
+  mp4Ara = [],
+  highestQuality,
+  controlBar,
+  newElement = document.createElement('div'),
+  newLink = document.createElement('a'),
+  newImage = document.createElement('img');
+  console.log('mediainfo',myPlayer.mediainfo);
+  console.log('1120');
+  myPlayer.on('loadstart',function(){
+    console.log('mediainfo',myPlayer.mediainfo);
+    // Extract name for use in download URL
     videoName = myPlayer.mediainfo['name'];
+    // Assign renditions to a variable and find number of renditions
     rendtionsAra = myPlayer.mediainfo.sources;
     totalRenditions = rendtionsAra.length;
+    // Get only MP4 rendtions into new array
     for (var i = 0; i < totalRenditions; i++) {
       if (rendtionsAra[i].container === "MP4" && rendtionsAra[i].hasOwnProperty('src')) {
         mp4Ara.push(rendtionsAra[i]);
       };
     };
-    mp4Ara.sort( function (a,b){
+    // Sort MP4 array
+    mp4Ara.sort(function (a, b) {
       return b.size - a.size;
     });
-    highestQuality= mp4Ara[0].src;
-      
-    newElement = document.createElement('div');
+    // Assign largest rendition to a variable for use in URL
+    highestQuality = mp4Ara[0].src;
+    // Assign id to div for icon
     newElement.id = 'downloadButton';
-    newElement.className = 'downloadStyle vjs-control'; 
-    newElement.innerHTML = "<a href='" + highestQuality + "' download='" + videoName + "'><img src='http://solutions.brightcove.com/bcls/brightcove-player/download-video/file-download.png' /></a>";
-    controlBar = document.getElementsByClassName('vjs-control-bar');
-    controlBar[0].appendChild(newElement);
+    // Assign properties to elements and assign to parents
+    newImage.setAttribute('src', 'http://solutions.brightcove.com/bcls/brightcove-player/download-video/file-download.png');
+    newLink.setAttribute('href', highestQuality + " download=" + videoName);
+    newLink.appendChild(newImage);
+    newElement.appendChild(newLink);
+    // Get control bar and insert before elements
+    // Remember that getElementsByClassName() returns an array
+    controlBar = document.getElementsByClassName('vjs-control-bar')[0];
+    // Insert the icon div in proper location
+    controlBar.appendChild(newElement);
   });
 });

f4fd6ae678bed4b5193393ab9dc8f0c686606b95
rcrooks <rcrooks@brightcove.com>
2016-07-14 06:09:52 -0400

    WIP DI status

--- di-api/callbacks-di.php
+++ di-api/callbacks-di.php
@@ -1,6 +1,8 @@
 <?php
 // var to log errors, if any
 $problem = "No errors";
+// var to store current video index
+$videoIndex = -1;
 // get the data file contents and parse them
 try {
 	$notificationData    = file_get_contents('di.json');
@@ -11,7 +13,7 @@ try {
 }
 
 // function to get index of video object if it already exists
-function doesVideoExist($videoId)
+function getIndex($videoId)
 {
     $idx = -1;
     for($i = 0; $i < count($notificationDataDecoded); $i++) {
@@ -23,6 +25,24 @@ function doesVideoExist($videoId)
     return $idx;
 }
 
+// function to make new video notification object
+function makeNotificationObject($videoId) {
+    $object = new stdClass();
+    $object->videoId = $videoId;
+    $object->complete = false;
+    $object->mp4Renditions = 0;
+    $object->hlsRenditions = 0;
+    $object->dashRenditions = 0;
+    $object->hdsRenditions = 0;
+    $object->smoothismRenditions = 0;
+    $object->hlsManifests = 0;
+    $object->dashManifests = 0;
+    $object->hdsManifests = 0;
+    $object->smoothManifests = 0;
+    return $object;
+}
+
+
 try {
 	$json    = file_get_contents('php://input');
 	$decoded = json_decode($json, true);
@@ -33,16 +53,16 @@ try {
 
 // Begin by extracting the useful parts of the notification
 
-if (isset($decoded["entity"])) {
+if (isset($decoded["videoId"])) {
 	$videoId = $decoded["videoId"];
+	// check data to see if object already exists; if not create one
+	$videoIndex = getIndex($videoId);
+	if ($videoIndex < 0) {
+        	makeNotificationObject($videoId);
+        	$videoIndex = getIndex($videoId);
+	}
 } else {
 	$videoId = null;
-}
-
-if (isset($decoded["entityType"])) {
-	$entityType = $decoded["entityType"];
-} else {
-	$entityType = null;
 }
 
 if (isset($decoded["status"])) {
@@ -51,29 +71,43 @@ if (isset($decoded["status"])) {
 	$status = null;
 }
 
-if (isset($decoded["action"])) {
-	$action = $decoded["action"];
+if (isset($decoded["entityType"])) {
+	$entityType = $decoded["entityType"];
+	// see if the entity type is ASSET and only proceed if the status is SUCCESS
+	if ($entityType == "ASSET" && $status == "SUCCESS") {
+    		// see if there's a profileRefId
+    		if (isset($decoded["profileRefId"])) {
+            	$profileRefId = $decoded["profileRefId"];
+            	// check for manifest or rendition types
+            	// only checking for HLS manifests here, because that's all the sample app will be seeing
+            	// but you could add additional checks for other types
+            	// also, only checking for MP4 or HLS renditions with standard profile reference ids,
+            	// because that's all this app will see
+            	// but you can check for other or additional reference id substrings based on the ingest profile(s)
+            	// you are using
+            	if ($profileRefId == "HlsManifest") {
+                	// increment the hsl manifest count in the notificaitons object
+                	$notificationDataDecoded[$videoIndex]->hlsManifests++;
+            	} elseif (substr($profileRefId, 0, 2) == "mp") {
+                	$notificationDataDecoded[$videoId]->mp4Renditions++;
+            	} elseif (substr($profileRefId, 0, 2) == "ts") {
+                	$notificationDataDecoded[$videoId]->hlsRenditions++;
+            	}
+        }
+        // if the entity type is TITLE and status is success, mark this on complete
+	} elseif ($entityType = "TITLE" && $status == "SUCCESS") {
+    	    $notificationDataDecoded[$videoId]->complete = true;
+	}
+	// all relevant information should be written
+	// now we'll replace the contents of di.json with what we have
+	file_put_contents('di.json', json_encode($notificationDataDecoded));
+
 } else {
-	$action = null;
+	$entityType = null;
 }
 
-if (isset($decoded["profileRefId"])) {
-	$profileRefId = $decoded["profileRefId"];
-} else {
-	$profileRefId = null;
-}
-
-// if notification is for completed title, act
-
-if (($entityType == 'TITLE') && ($action == 'CREATE') && ($status == 'SUCCESS')) {
-	$newLine = "\nvideoIdArray.unshift(".$entityId.");";
-	// Tell PHP where it can find the log file and tell PHP to open it
-	// and add the string we created earlier to it.
-	$logFileLocation = "video-ids.js";
-	$fileHandle      = fopen($logFileLocation, 'a') or die("-1");
-	fwrite($fileHandle, $newLine);
-	fclose($fileHandle);
-}
 
 echo "Dynamic Ingest callback app is running";
+
+
 ?>

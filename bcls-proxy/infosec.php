9d07b5428fff67213790eb4742c60975ab285fbe
rcrooks <rcrooks@brightcove.com>
2016-07-22 08:31:16 -0400

    php security stuff

--- bcls-proxy/infosec.php
+++ bcls-proxy/infosec.php
@@ -1,3 +1,9 @@
 <?php
-var_dump($_GET);
+
+
+if (strpos($_SERVER['HTTP_REFERER'], 'solutions.brightcove.com') == false && strpos($_SERVER['HTTP_REFERER'], 'docs.brightcove.com') == false ) {
+    exit('Only requests from http://docs.brightcove.com or http:solutions.brightcove.com are accepted by this proxy');
+} else {
+    echo $_SERVER['HTTP_REFERER'];
+}
 ?>

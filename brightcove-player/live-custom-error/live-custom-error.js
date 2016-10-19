a9f65aca61ee7a84c8d01c495581ba36e3b30482
rcrooks <rcrooks@brightcove.com>
2016-10-19 07:58:57 -0400

    fix for plugin

--- brightcove-player/live-custom-error/live-custom-error.js
+++ brightcove-player/live-custom-error/live-custom-error.js
@@ -2,10 +2,10 @@
  * Live Custom Error
  */
 videojs.plugin('liveCustomError', function() {
-    
+
     var myPlayer = this,
         showOverlay = false;
-        
+
         myPlayer.errors({
             'errors': {
                 '2': {
@@ -13,26 +13,26 @@ videojs.plugin('liveCustomError', function() {
                     'type': 'MEDIA_UNAVAILABLE',
                 }
             }
-        }); 
-        
+        });
+
         myPlayer.on("error", handleError);
-        
+
         function handleError(err) {
             var errNo = myPlayer.error().code;
             var duration = myPlayer.duration();
 
-            if ((errNo == '2') && (duration=='0')) {
+            if (((errNo == '2') && (duration =='0')) || (errNo == '4') && (duration == '0')) {
                 showOverlay = true;
             }
-		}	
-     
-		myPlayer.ready(function(){
+        }
+
+        myPlayer.ready(function(){
             if (showOverlay) {
-                // show overlay image 
-                myPlayer.removeClass("hide-overlay"); 
+                // show overlay image
+                myPlayer.removeClass("hide-overlay");
             } else {
                 // hide overlay image
                 myPlayer.addClass("hide-overlay");
             }
-		});
+        });
 });

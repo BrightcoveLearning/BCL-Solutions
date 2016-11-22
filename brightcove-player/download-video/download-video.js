49f34365a97a5340acebe7bed20cc0e2c45e8c4b
Matthew Boles <mboles@brightcove.com>
2016-11-22 14:33:54 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	brightcove-player/live-custom-error/live-custom-error.js

--- brightcove-player/download-video/download-video.js
+++ brightcove-player/download-video/download-video.js
@@ -1,34 +1,50 @@
 videojs.plugin('downloadVideoPlugin', function() {
+
+  // Create variables and new div, anchor and image for download icon
   var myPlayer = this,
-    videoID,
-    videoName,
-    totalRenditions,
-    mp4Ara=[],
-    highestQuality,
-    controlBar,
-    newElement;
-    
-  videoID = myPlayer.options()['data-video-id'];
-  myPlayer.catalog.getVideo(videoID, function(error, video) {
-    myPlayer.catalog.load(video);
-    videoName = myPlayer.mediainfo['name'];
-    rendtionsAra = myPlayer.mediainfo.sources;
-    totalRenditions = rendtionsAra.length;
-    for (var i = 0; i < totalRenditions; i++) {
-      if (rendtionsAra[i].container === "MP4" && rendtionsAra[i].hasOwnProperty('src')) {
-        mp4Ara.push(rendtionsAra[i]);
+      videoName,
+      totalRenditions,
+      mp4Ara=[],
+      highestQuality,
+      controlBar,
+      newElement = document.createElement('div'),
+      newLink = document.createElement('a'),
+	  newImage = document.createElement('img');
+
+  myPlayer.on('loadstart',function(){
+      // Get video name and the MP4 renditions
+      console.log('mediainfo', myPlayer.mediainfo);
+      videoName = myPlayer.mediainfo['name'];
+      rendtionsAra = myPlayer.mediainfo.sources;
+      totalRenditions = rendtionsAra.length;
+      for (var i = 0; i < totalRenditions; i++) {
+          if (rendtionsAra[i].container === "MP4" && rendtionsAra[i].hasOwnProperty('src')) {
+              mp4Ara.push(rendtionsAra[i]);
+          };
       };
-    };
-    mp4Ara.sort( function (a,b){
-      return b.size - a.size;
-    });
-    highestQuality= mp4Ara[0].src;
-      
-    newElement = document.createElement('div');
-    newElement.id = 'downloadButton';
-    newElement.className = 'downloadStyle vjs-control'; 
-    newElement.innerHTML = "<a href='" + highestQuality + "' download='" + videoName + "'><img src='http://solutions.brightcove.com/bcls/brightcove-player/download-video/file-download.png' /></a>";
-    controlBar = document.getElementsByClassName('vjs-control-bar');
-    controlBar[0].appendChild(newElement);
-  });
+      // Sort the renditions from highest to lowest
+      mp4Ara.sort( function (a,b){
+          return b.size - a.size;
+      });
+	  // Set the highest rendition
+      highestQuality= mp4Ara[0].src;
+
+	  // Assign id and classes to div for icon
+      newElement.id = 'downloadButton';
+      newElement.className = 'downloadStyle vjs-control';
+
+      // Assign properties to elements and assign to parents
+      newImage.setAttribute('src','http://solutions.brightcove.com/bcls/brightcove-player/download-video/file-download.png');
+	  newLink.setAttribute('href',highestQuality);
+	  newLink.setAttribute('download',videoName);
+	  newLink.appendChild(newImage);
+	  newElement.appendChild(newLink);
+
+	  // Get the spacer element
+      spacer = myPlayer.controlBar.customControlSpacer.el();
+	  // Set the content of the spacer to be right justified
+    spacer.setAttribute("style", "justify-content: flex-end;");
+    // Place the new element in the spacer
+    spacer.appendChild(newElement);
+  })
 });

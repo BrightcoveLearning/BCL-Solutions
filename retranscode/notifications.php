43d1525e30f9c1e5d74d95b5be7d4c38d8e7d2a7
rcrooks <rcrooks@brightcove.com>
2017-01-10 12:17:10 -0500

    update to transcoder app

--- retranscode/notifications.php
+++ retranscode/notifications.php
@@ -11,7 +11,6 @@ try {
 
 // if notification received turn notification into pretty printed JSON
 if ($decoded) {
-    $notification = json_encode($decoded, JSON_PRETTY_PRINT);
     $account_id = $decoded['accountId'];
 
     // if notification received job complete, update the job count file
@@ -20,6 +19,7 @@ if ($decoded) {
         $job_count_data    = file_get_contents($job_count_file);
         $job_count_decoded = json_decode($job_count_data);
         if ($decoded['entityType'] == 'TITLE') {
+            // check action as well as status for Bolt compatibility
             if ($decoded['status'] == 'SUCCESS' && $decoded['action'] == 'CREATE') {
                 $job_count_decoded->job_count--;
             } elseif ($decoded['status'] == 'FAILED') {
@@ -33,12 +33,18 @@ if ($decoded) {
 
         $logFileLocation = $account_id.'_notifications.txt';
         $current_log = file_get_contents($logFileLocation);
-        $logEntry = $notification."\n";
+        if (!current_log) {
+            $current_log_decoded = array();
+        } else {
+            $current_log_decoded = json_decode($current_log);
+        }
+        $current_log_decoded.push($decoded);
+        $current_log = json_encode($current_log_decoded, JSON_PRETTY_PRINT);
 
         // Lastly, tell PHP where it can find the log file and tell PHP to open it
 
         $fileHandle      = fopen($logFileLocation, 'a');
-        fwrite($fileHandle, $logEntry);
+        fwrite($fileHandle, $current_log);
         fclose($fileHandle);
     }
 }

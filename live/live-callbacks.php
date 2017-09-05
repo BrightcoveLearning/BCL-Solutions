34b4339b66c180f364128cbde9a5fffffc5b8d3a
rcrooks <rcrooks@brightcove.com>
2017-09-05 08:46:31 -0400

    update notification handler

--- live/live-callbacks.php
+++ live/live-callbacks.php
@@ -20,12 +20,10 @@ $logEntry = $notification.
 // Lastly, tell PHP where it can find the log file and tell PHP to open it
 // and add the string we created earlier to it.
 // 2016-09-15: turning off CMS API notifications for now
-if ($notificationType !== 'CMS API') {
     $logFileLocation = "live-log.txt";
     $fileHandle      = fopen($logFileLocation, 'a') or die("-1");
     fwrite($fileHandle, $logEntry);
     fclose($fileHandle);
-}
 
 // line below is displayed when you browse the app directly
 echo "Live callback app is running";

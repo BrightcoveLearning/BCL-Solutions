406368eab65312b0eb81f60a9e01ba5caf7ac072
Robert Crooks <rcrooks@brightcove.com>
2018-01-01 15:11:54 -0500

    small fixes

--- bcls-proxy/live-proxy.php
+++ bcls-proxy/live-proxy.php
@@ -44,12 +44,9 @@ if ($_POST["requestType"]) {
 $needle = '.com';
 $endapi = strpos($_POST["url"], $needle) + 4;
 
-if (strpos($_POST["url"], 'data.brightcove.co.jp')) {
-    $endapi = strpos($_POST["url"], $needle) + 6;
-}
 $nextChar = substr($_POST['url'], $endapi, 1);
 
-if (strpos($_POST["url"], 'api.brightcove.com') == false && strpos($_POST["url"], 'api.bcovlive.io') == false && strpos($_POST["url"], 'data.brightcove.co.jp') == false && strpos($_POST["url"], 'data.brightcove.com') == false) {
+if (strpos($_POST["url"], 'api.brightcove.com') == false && strpos($_POST["url"], 'api.bcovlive.io') == false) {
     exit('{"ERROR":"Only requests to Brightcove APIs are accepted by this proxy"}');
 } else if ($nextChar !== '/' && $nextChar !== '?') {
     exit('{"ERROR": "There was a problem with your API request - please check the URL"}');

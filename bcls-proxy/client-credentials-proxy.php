c27eff459aeaeb1b1040239bfc99d5e5bd243ba0
Robert Crooks <rcrooks@brightcove.com>
2018-01-04 09:37:15 -0500

    creating new proxy

---
+++ bcls-proxy/client-credentials-proxy.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * access-token-proxy.php - proxy for Brightcove RESTful APIs
+ * gets an access token and returns the whole response
+ * Accessing:
+ *         (note you should *always* access the proxy via HTTPS)
+ *     Method: POST
+ *
+ * @post {string} client_id - OAuth2 client id with sufficient permissions for the request
+ * @post {string} client_secret - OAuth2 client secret with sufficient permissions for the request
+ *
+ * @returns {string} $response - JSON response received from the OAuth API
+ */
+
+// security checks
+if (strpos($_SERVER['HTTP_REFERER'], 'solutions.brightcove.com') == false && strpos($_SERVER['HTTP_REFERER'], 'ondemand.brightcovelearning.com') == false && strpos($_SERVER['HTTP_REFERER'], 'video.brightcovelearning.com') == false && strpos($_SERVER['HTTP_REFERER'], 's.codepen.io') == false && strpos($_SERVER['HTTP_REFERER'], 'fiddle.jshell.net') == false && strpos($_SERVER['HTTP_REFERER'], 'players.brightcove.net') == false && strpos($_SERVER['HTTP_REFERER'], 'support.brightcove.com') == false && strpos($_SERVER['HTTP_REFERER'], 'master-7rqtwti-6sglloa4yrkti.us.platform.sh') == false) {
+    exit('{"ERROR":"Only requests from http://docs.brightcove.com or http:solutions.brightcove.com are accepted by this proxy"}');
+}
+
+// CORS enablement and other headers
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json");
+header("X-Content-Type-Options: nosniff");
+header("X-XSS-Protection");
+
+// set up request for access token
+$data = array();
+
+$client_id     = $_POST["client_id"];
+$client_secret = $_POST["client_secret"];
+$auth_string   = "{$client_id}:{$client_secret}";
+$request       = "https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials";
+$ch            = curl_init($request);
+curl_setopt_array($ch, array(
+        CURLOPT_POST           => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_SSL_VERIFYPEER => FALSE,
+        CURLOPT_USERPWD        => $auth_string,
+        CURLOPT_HTTPHEADER     => array(
+            'Content-type: application/x-www-form-urlencoded',
+        ),
+        CURLOPT_POSTFIELDS => $data
+    ));
+$response = curl_exec($ch);
+curl_close($ch);
+
+// Check for errors
+if ($response === FALSE) {
+    die(curl_error($ch));
+    echo 'An error occurred';
+} else {
+  echo $response;
+}
+
+?>

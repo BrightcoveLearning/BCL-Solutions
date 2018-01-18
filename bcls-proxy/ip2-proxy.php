d85f43e5845079c049f95eb865be8d6f30db251a
Robert Crooks <rcrooks@brightcove.com>
2018-01-18 09:53:21 -0500

    proxy update

--- bcls-proxy/ip2-proxy.php
+++ bcls-proxy/ip2-proxy.php
@@ -3,7 +3,7 @@
  * bcls-proxy.php - proxy for Brightcove RESTful APIs
  * gets an access token, makes the request, and returns the response
  * Accessing:
- *     (note you should *always* access the proxy via HTTPS)
+ *         (note you should *always* access the proxy via HTTPS)
  *     Method: POST
  *
  * @post {string} url - the URL for the API request
@@ -19,20 +19,24 @@
 if (strpos($_SERVER['HTTP_REFERER'], 'solutions.brightcove.com') == false && strpos($_SERVER['HTTP_REFERER'], 'ondemand.brightcovelearning.com') == false && strpos($_SERVER['HTTP_REFERER'], 'video.brightcovelearning.com') == false && strpos($_SERVER['HTTP_REFERER'], 's.codepen.io') == false && strpos($_SERVER['HTTP_REFERER'], 'fiddle.jshell.net') == false && strpos($_SERVER['HTTP_REFERER'], 'players.brightcove.net') == false && strpos($_SERVER['HTTP_REFERER'], 'support.brightcove.com') == false && strpos($_SERVER['HTTP_REFERER'], 'master-7rqtwti-6sglloa4yrkti.us.platform.sh') == false) {
     exit('{"ERROR":"Only requests from http://docs.brightcove.com or http:solutions.brightcove.com are accepted by this proxy"}');
 }
-// CORS enablement
+// CORS enablement and other headers
 header("Access-Control-Allow-Origin: *");
-echo $_POST;
+header("Content-type: application/json");
+header("X-Content-Type-Options: nosniff");
+header("X-XSS-Protection");
+
 // set up request for access token
 $data = array();
+
 if ($_POST["client_id"]) {
     $client_id = $_POST["client_id"];
 } else {
-    $client_id = '3e23bbec-59b8-4861-b5ba-7c26e110a746';
+    $client_id = 'b10631d3-7597-4be8-b8b5-dce142f81006';
 }
 if ($_POST["client_secret"]) {
     $client_secret = $_POST["client_secret"];
 } else {
-    $client_secret = 'quNdrH07IVoG8yZxSFsCySWmtvUuWfPYyzeg1Nil7Md7VpQ50A3KVV4eeMrZSR7FdeZA_3JS5jV9pBBI0skwWA';
+    $client_secret = 'h1dbPZCMFsloMCiXprlGDvdDR7QXtcw9alyocJ1ShDfLZ5QxqBqb9u_5gGcU6mlyA1PbbG6ABYS1FMDVE4JNDQ';
 }
 $auth_string   = "{$client_id}:{$client_secret}";
 $request       = "https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials";
@@ -60,39 +64,33 @@ $responseData = json_decode($response, TRUE);
 $access_token = $responseData["access_token"];
 
 // set up the API call
-//  55525f84e4b08e946aa03281 ,"id":"5591b61ce4b072e9641b830e"
 // get data
 if ($_POST["requestBody"]) {
     $data = json_decode($_POST["requestBody"]);
-} else {
-    $data = json_decode('{"account_id":57838016001,"default_profile_id":"smart-player-transition"}');
 }
 // get request type or default to GET
 if ($_POST["requestType"]) {
     $method = $_POST["requestType"];
 } else {
-    $method = "PUT";
-}
-
-// get the URL and authorization info from the form data
-if ($_POST["url"]) {
-    $request = $_POST["url"];
-} else {
-    $request = "https://ingestion.api.brightcove.com/v1/accounts/57838016001/configuration";
+    $method = "GET";
 }
 
 // more security checks
 $needle = '.com';
-$endapi = strpos($request, $needle) + 4;
-$nextChar = substr($request, $endapi, 1);
-if (strpos($request, 'api.brightcove.com') == false) {
+$endapi = strpos($_POST["url"], $needle) + 4;
+
+if (strpos($_POST["url"], 'data.brightcove.co.jp')) {
+    $endapi = strpos($_POST["url"], $needle) + 6;
+}
+$nextChar = substr($_POST['url'], $endapi, 1);
+
+if (strpos($_POST["url"], 'api.brightcove.com') == false && strpos($_POST["url"], 'data.brightcove.co.jp') == false && strpos($_POST["url"], 'data.brightcove.com') == false) {
     exit('{"ERROR":"Only requests to Brightcove APIs are accepted by this proxy"}');
 } else if ($nextChar !== '/' && $nextChar !== '?') {
     exit('{"ERROR": "There was a problem with your API request - please check the URL"}');
 }
-
-
-
+// get the URL and authorization info from the form data
+$request = $_POST["url"];
 //send the http request
 if ($_POST["requestBody"]) {
   $ch = curl_init($request);
@@ -122,7 +120,7 @@ if ($_POST["requestBody"]) {
   $response = curl_exec($ch);
   curl_close($ch);
 }
-// var_dump($data);
+
 // Check for errors
 if ($response === FALSE) {
     $logEntry = "\nError:\n".
@@ -141,7 +139,7 @@ if ($response === FALSE) {
 // return the response to the AJAX caller
 // $responseDecoded = json_decode($response);
 // if (!isset($responseDecoded)) {
-// 	$response = '{null}';
+//     $response = '{null}';
 // }
 echo $response;
 ?>

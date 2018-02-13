31e415e68c39467b81892e490bd398e5efde2c50
Robert Crooks <rcrooks@brightcove.com>
2018-02-13 15:15:49 -0500

    wip

--- bcls-proxy/doc-samples-proxy-v2.php
+++ bcls-proxy/doc-samples-proxy-v2.php
@@ -21,7 +21,7 @@
 
 // security checks
 if (strpos($_SERVER['HTTP_REFERER'], 'support.brightcove.com') == false && strpos($_SERVER['HTTP_REFERER'], 'solutions.brightcove.com') == false && strpos($_SERVER['HTTP_REFERER'], 'ondemand.brightcovelearning.com') == false && strpos($_SERVER['HTTP_REFERER'], 'video.brightcovelearning.com') == false && strpos($_SERVER['HTTP_REFERER'], 's.codepen.io') == false && strpos($_SERVER['HTTP_REFERER'], 'fiddle.jshell.net') == false && strpos($_SERVER['HTTP_REFERER'], 'players.brightcove.net') == false && strpos($_SERVER['HTTP_REFERER'], 'master-7rqtwti-6sglloa4yrkti.us.platform.sh') == false) {
-    exit('{"ERROR":"Only requests from brightcove sites are accepted by this proxy"}');
+    exit('{"ERROR":"Only requests from http:solutions.brightcove.com are accepted by this proxy"}');
 }
 // CORS enablement and other headers
 header("Access-Control-Allow-Origin: *");

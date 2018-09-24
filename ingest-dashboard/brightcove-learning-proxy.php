afad953f16dd9edc09b178adee73864c5cfa20f1
Robert Crooks <rcrooks@brightcove.com>
2018-09-24 09:54:55 -0400

    wip

--- ingest-dashboard/brightcove-learning-proxy.php
+++ ingest-dashboard/brightcove-learning-proxy.php
@@ -20,79 +20,121 @@
  header("X-Content-Type-Options: nosniff");
  header("X-XSS-Protection");
 
-// set up request for access token
-$data = array();
-/*
- * change the values below to use this proxy with a different account
- * the client_id and client_secret must have read/write permissions for the Dynamic Ingest and CMS APIs
- */
-$client_id     = "3e23bbec-59b8-4861-b5ba-7c26e110a746";
-$client_secret = "quNdrH07IVoG8yZxSFsCySWmtvUuWfPYyzeg1Nil7Md7VpQ50A3KVV4eeMrZSR7FdeZA_3JS5jV9pBBI0skwWA";
-$auth_string   = "{$client_id}:{$client_secret}";
-$request       = "https://oauth.brightcove.com/v3/access_token?grant_type=client_credentials";
-$ch            = curl_init($request);
-curl_setopt_array($ch, array(
-        CURLOPT_POST           => TRUE,
-        CURLOPT_RETURNTRANSFER => TRUE,
-        CURLOPT_SSL_VERIFYPEER => FALSE,
-        CURLOPT_USERPWD        => $auth_string,
-        CURLOPT_HTTPHEADER     => array(
-            'Content-type: application/x-www-form-urlencoded',
-        ),
-        CURLOPT_POSTFIELDS => $data
-    ));
-$response = curl_exec($ch);
-curl_close($ch);
+ // default account values
+ $default_client_id     = '3e23bbec-59b8-4861-b5ba-7c26e110a746';
+ $default_client_secret = 'quNdrH07IVoG8yZxSFsCySWmtvUuWfPYyzeg1Nil7Md7VpQ50A3KVV4eeMrZSR7FdeZA_3JS5jV9pBBI0skwWA';
 
-// Check for errors
-if ($response === FALSE) {
-    die(curl_error($ch));
-}
+ // get request body
+ $requestData = json_decode(file_get_contents('php://input'));
 
-// Decode the response
-$responseData = json_decode($response, TRUE);
-$access_token = $responseData["access_token"];
+ // set up access token request
+ if ($requestData->client_id) {
+     $client_id = $requestData->client_id;
+ } else {
+     // default to the id for all permissions for most BCLS accounts
+     $client_id = $default_client_id;
+ }
+ if ($requestData->client_secret) {
+     $client_secret = $requestData->client_secret;
+ } else {
+     // default to the secret for all permissions for most BCLS accounts
+     $client_secret = $default_client_secret;
+ }
 
-// set up the API call
-// get data
-if ($_POST["requestBody"]) {
-    $data = json_decode($_POST["requestBody"]);
-} else {
-    $data = array();
-}
-// get request type or default to GET
-if ($_POST["requestType"]) {
-    $method = $_POST["requestType"];
-} else {
-    $method = "GET";
-}
+ $auth_string = "{$client_id}:{$client_secret}";
+ $request     = "https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials";
+ $ch          = curl_init($request);
+ curl_setopt_array($ch, array(
+         CURLOPT_POST           => TRUE,
+         CURLOPT_RETURNTRANSFER => TRUE,
+         CURLOPT_SSL_VERIFYPEER => FALSE,
+         CURLOPT_USERPWD        => $auth_string,
+         CURLOPT_HTTPHEADER     => array(
+             'Content-type: application/x-www-form-urlencoded',
+         ),
+     ));
+ $response = curl_exec($ch);
+ curl_close($ch);
 
-// get the URL and authorization info from the form data
-$request = $_POST["url"];
+ // Check for errors
+ if ($response === FALSE) {
+     die(curl_error($ch));
+ }
 
-//send the http request
-$ch = curl_init($request);
-curl_setopt_array($ch, array(
-        CURLOPT_CUSTOMREQUEST  => $method,
-        CURLOPT_RETURNTRANSFER => TRUE,
-        CURLOPT_SSL_VERIFYPEER => FALSE,
-        CURLOPT_HTTPHEADER     => array(
-            'Content-type: application/json',
-            "Authorization: Bearer {$access_token}",
-        ),
-        CURLOPT_POSTFIELDS => json_encode($data)
-    ));
-$response = curl_exec($ch);
-curl_close($ch);
+ // Decode the response
+ $responseData = json_decode($response, TRUE);
+ $access_token = $responseData["access_token"];
 
-// Check for errors
-if ($response === FALSE) {
-    echo "Error: "+$response;
-    die(curl_error($ch));
-}
+ // get request type or default to GET
+ if ($requestData->requestType) {
+     $method = $requestData->requestType;
+ } else {
+     $method = "GET";
+ }
 
-// Decode the response
-// $responseData = json_decode($response, TRUE);
-// return the response to the AJAX caller
-echo $response;
-?>
+ // more security checks
+ $needle = '.com';
+ $endapi = strpos($requestData->url, $needle) + 4;
+
+ if (strpos($requestData->url, 'data.brightcove.co.jp')) {
+     $endapi = strpos($requestData->url, $needle) + 6;
+ }
+ $nextChar = substr($requestData->url, $endapi, 1);
+
+ if (strpos($requestData->url, 'api.brightcove.com') == false && strpos($requestData->url, 'data.brightcove.co.jp') == false && strpos($requestData->url, 'data.brightcove.com') == false) {
+     exit('{"ERROR":"Only requests to Brightcove APIs are accepted by this proxy"}');
+ } else if ($nextChar !== '/' && $nextChar !== '?') {
+     exit('{"ERROR": "There was a problem with your API request - please check the URL"}');
+ }
+ // get the URL and authorization info from the form data
+ $request = $requestData->url;
+ //send the http request
+ if ($requestData->requestBody) {
+   $ch = curl_init($request);
+   curl_setopt_array($ch, array(
+     CURLOPT_CUSTOMREQUEST  => $method,
+     CURLOPT_RETURNTRANSFER => TRUE,
+     CURLOPT_SSL_VERIFYPEER => FALSE,
+     CURLOPT_HTTPHEADER     => array(
+       'Content-type: application/json',
+       "Authorization: Bearer {$access_token}",
+     ),
+     CURLOPT_POSTFIELDS => $requestData->requestBody
+   ));
+   $response = curl_exec($ch);
+   curl_close($ch);
+ } else {
+   $ch = curl_init($request);
+   curl_setopt_array($ch, array(
+     CURLOPT_CUSTOMREQUEST  => $method,
+     CURLOPT_RETURNTRANSFER => TRUE,
+     CURLOPT_SSL_VERIFYPEER => FALSE,
+     CURLOPT_HTTPHEADER     => array(
+       'Content-type: application/json',
+       "Authorization: Bearer {$access_token}",
+     )
+   ));
+   $response = curl_exec($ch);
+   curl_close($ch);
+ }
+
+ // Check for errors and log them if any
+ // note that logging will fail unless
+ // the file log.txt exists in the same
+ // directory as the proxy and is writable
+
+ if ($response === FALSE) {
+     $logEntry = "\nError:\n".
+     "\n".date("Y-m-d H:i:s")." UTC \n"
+     .$response;
+     $logFileLocation = "log.txt";
+     $fileHandle      = fopen($logFileLocation, 'a') or die("-1");
+     fwrite($fileHandle, $logEntry);
+     fclose($fileHandle);
+     echo "Error: there was a problem with your API call"+
+     die(curl_error($ch));
+ }
+
+ // return the response to the AJAX caller
+ echo $response;
+ ?>

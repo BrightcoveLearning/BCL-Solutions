4a74a227db8d42ca50b530dc579722a760192577
Robert Crooks <rcrooks@brightcove.com>
2018-09-27 15:34:24 -0400

    wip

--- di-api/di-callback-app.php
+++ di-api/di-callback-app.php
@@ -13,9 +13,7 @@ $notification = json_encode($decoded, JSON_PRETTY_PRINT);
 
 
 
-$logEntry = $notification.
-"\nErrors receiving notificatons: ".$problem.
-"\n-------------------------------\n";
+$logEntry = $notification.",\n"
 
 // Lastly, tell PHP where it can find the log file and tell PHP to open it
 // and add the string we created earlier to it.

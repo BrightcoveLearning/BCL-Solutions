303178372139bbc45ffa4733d84aa05f2f6de6aa
Robert Crooks <rcrooks@brightcove.com>
2018-09-27 14:06:21 -0400

    add missing function

--- ingest-dashboard/di-tester.js
+++ ingest-dashboard/di-tester.js
@@ -43,6 +43,23 @@ var BCLS = (function(window, document) {
   }
 
   /**
+   * determines whether specified item is in an array
+   *
+   * @param {array} array to check
+   * @param {string} item to check for
+   * @return {boolean} true if item is in the array, else false
+   */
+  function arrayContains(arr, item) {
+      var i,
+          iMax = arr.length;
+      for (i = 0; i < iMax; i++) {
+          if (arr[i] === item) {
+              return true;
+          }
+      }
+      return false;
+  }
+  /**
    * find the dynamic delivery profiles
    * @param {array} profiles the profiles array to search
    */

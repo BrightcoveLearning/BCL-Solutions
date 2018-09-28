b5c556f850710358fb94628cd97222fc02c91e1f
Robert Crooks <rcrooks@brightcove.com>
2018-09-28 15:05:25 -0400

    wip

--- ingest-dashboard/di-log.js
+++ ingest-dashboard/di-log.js
@@ -12,7 +12,7 @@ var BCLS = ( function (videoIdArray) {
         proxyURL = './brightcove-learning-proxy.php',
         videoDataArray = [],
         videoDataItem = {},
-        requestOptions = {},
+        options = {},
         currentVideo;
 
 
@@ -26,20 +26,34 @@ var BCLS = ( function (videoIdArray) {
                 item = videoDataArray[j];
                 logBody.innerHTML += '<tr><td>' + item.id + '</td><td>' + item.name + '</td><td>' + item.reference_id + '</td><td>' + item.sources + '</td></tr>';
             }
-        };
+        }
 
         // function to set up video data request
         function setVideoRequestOptions() {
-            requestOptions = {};
-            requestOptions.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/videos/' + currentVideo);
-            console.log(requestOptions.url);
-            submitRequest(requestOptions, proxyURL, 'video');
+            options = {};
+            options.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/videos/' + currentVideo);
+            options.proxyURL = proxyURL;
+            console.log(options.url);
+            submitRequest(options, function{response} {
+              var o = {};
+              response = JSON.parse(response);
+              o.id = response.id;
+              o.reference_id = response.reference_id;
+              o.name = response.name;
+              videoDataArray.push(o);
+              setSourcesRequestOptions();
+            });
         };
         // function to set up video sources request
         function setSourcesRequestOptions() {
-            requestOptions.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/videos/' + currentVideo + '/sources';
-            submitRequest(requestOptions, proxyURL, 'sources');
-        };
+            options.url = 'https://cms.api.brightcove.com/v1/accounts/' + account_id + '/videos/' + currentVideo + '/sources';
+            options.proxyURL = proxyURL;
+
+            submitRequest(options, function(response) {
+              response = JSON.parse(response);
+              videoDataArray[videoNumber].sources = response.length;
+            });
+        }
 
         // initiates the cms api requests
         function getVideoInfo() {
@@ -51,69 +65,57 @@ var BCLS = ( function (videoIdArray) {
             }
         };
 
-        // function to make the cms api requests
-        function submitRequest(options, url, type) {
+        /**
+         * send API request to the proxy
+         * @param  {Object} options for the request
+         * @param  {String} options.url the full API request URL
+         * @param  {String="GET","POST","PATCH","PUT","DELETE"} requestData [options.requestType="GET"] HTTP type for the request
+         * @param  {String} options.proxyURL proxyURL to send the request to
+         * @param  {String} options.client_id client id for the account (default is in the proxy)
+         * @param  {String} options.client_secret client secret for the account (default is in the proxy)
+         * @param  {JSON} [options.requestBody] Data to be sent in the request body in the form of a JSON string
+         * @param  {Function} [callback] callback function that will process the response
+         */
+        function makeRequest(options, callback) {
             var httpRequest = new XMLHttpRequest(),
-                requestData,
-                responseData,
-                parsedData,
-                getResponse = function () {
+                response,
+                requestParams,
+                dataString,
+                proxyURL    = options.proxyURL,
+                // response handler
+                getResponse = function() {
                     try {
                         if (httpRequest.readyState === 4) {
-                          if (httpRequest.status === 200) {
-                            responseData = httpRequest.responseText;
-                            switch (type) {
-                                case "video":
-                                videoDataItem = {};
-                                parsedData = JSON.parse(responseData);
-                                videoDataItem.id = parsedData.id;
-                                videoDataItem.reference_id = parsedData.reference_id;
-                                videoDataItem.name = parsedData.name;
-                                setSourcesRequestOptions();
-                                break;
-                                case "sources":
-                                parsedData = JSON.parse(responseData);
-                                if (parsedData.length > 0) {
-                                    videoDataItem.sources = parsedData.length;
-                                } else {
-                                    videoDataItem.sources = '<span style="color:#990000">No renditions - ingest failed!</span>';
+                            if (httpRequest.status >= 200 && httpRequest.status < 300) {
+                                response = httpRequest.responseText;
+                                // some API requests return '{null}' for empty responses - breaks JSON.parse
+                                if (response === '{null}') {
+                                    response = null;
                                 }
-
-                                videoDataArray.push(videoDataItem);
-
-                                videoNumber++;
-                                if (videoNumber < videoNumberMax) {
-                                    getVideoInfo();
-                                } else {
-                                    writeReport();
-                                }
-                                break;
-                            }
-                          } else {
-                            console.log("There was a problem with the request. Request returned " + httpRequest.status);
-                            if (type === 'video') {
-                                setVideoRequestOptions();
+                                // return the response
+                                callback(response);
                             } else {
-                                setSourcesRequestOptions();
+                                alert('There was a problem with the request. Request returned ' + httpRequest.status);
                             }
-                          }
                         }
-                      }
-                      catch(e) {
-                        console.log('Caught Exception: ' + e);
-                      }
+                    } catch (e) {
+                        alert('Caught Exception: ' + e);
+                    }
                 };
-            // set up request data
-            requestData = "url=" + encodeURIComponent(options.url) + "&requestType=GET";
+            /**
+             * set up request data
+             * the proxy used here takes the following request body:
+             * JSON.strinify(options)
+             */
             // set response handler
             httpRequest.onreadystatechange = getResponse;
             // open the request
-            httpRequest.open("POST", url);
-            // set headers
-            httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            httpRequest.open('POST', proxyURL);
+            // set headers if there is a set header line, remove it
             // open and send request
-            httpRequest.send(requestData);
-        };
+            httpRequest.send(JSON.stringify(options));
+        }
+
 
         // event handlers
         clearLogBtn.addEventListener('click', function () {

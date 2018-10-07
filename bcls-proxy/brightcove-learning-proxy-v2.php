f1f11d0a63122fb1793857a30e89bf74af1c779d
Robert Crooks <rcrooks@brightcove.com>
2018-10-07 11:43:19 -0400

    wip

--- bcls-proxy/brightcove-learning-proxy-v2.php
+++ bcls-proxy/brightcove-learning-proxy-v2.php
@@ -104,7 +104,7 @@ if ($requestData->requestBody) {
   curl_setopt_array($ch, array(
     CURLOPT_CUSTOMREQUEST  => $method,
     CURLOPT_RETURNTRANSFER => TRUE,
-    CURLOPT_SSL_VERIFYPEER => FALSE,
+    CURLOPT_SSL_VERIFYPEER => TRUE,
     CURLOPT_HTTPHEADER     => array(
       'Content-type: application/json',
       "Authorization: Bearer {$access_token}",

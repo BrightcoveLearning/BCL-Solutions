88fc06ddf78b42dd8703c43f6da145374efa7639
Robert Crooks <rcrooks@brightcove.com>
2019-04-30 09:33:56 -0400

    fix

--- tests/redirect/developers/documentation/test.php
+++ tests/redirect/developers/documentation/test.php
@@ -1,12 +1,12 @@
 <?php 
 
-$uri = $_SERVER[REQUEST_URI];
-$host = $_SERVER[HTTP_HOST];
+$uri = $_SERVER['REQUEST_URI'];
+$host = $_SERVER['HTTP_HOST'];
 if (strpos($uri, 'developers/documentation') == true) {
   $newUri = str_replace('developers/documentation', 'video-platform', $uri);
   $newUrl = 'https://'.$host.$newUri;
+  header("Location: $newUrl", true, 301); 
 }
-header("Location: $newUrl"); 
 var_dump($newUrl);
 ?>
 
